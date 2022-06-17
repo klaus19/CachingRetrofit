6e4a092eaa756731d25285c1573b84d23d141a8b
tejaskhartude <tejaskhartude@gmail.com>
2022-06-17 23:08:01 +0530

    updated

--- app/src/main/java/com/example/cachingretrofit/MainActivity.kt
+++ app/src/main/java/com/example/cachingretrofit/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.cachingretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cachingretrofit.api.ApiService
 
 class MainActivity : AppCompatActivity() {
+
+    var apiService:ApiService?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
     }
 }
